bd062f647d4758dc875a503cc3fc44c2c91af1f2
Gaunish <anigarg1999@gmail.com>
2022-01-27 22:36:57 -0500

    all

--- hw_1_ride_share/docker-deploy/web-app/ride/views.py
+++ hw_1_ride_share/docker-deploy/web-app/ride/views.py
@@ -6,6 +6,7 @@ import datetime
 from django.utils import timezone
 from django.views.generic import ListView, DetailView, CreateView
 
+
 def index(request):
     return render(request,'index.html')
 
@@ -14,6 +15,7 @@ def login(request):
     if request.method == 'POST':
         #Put the post data into form 
         form = Login(request.POST)
+
         #if valid data
         if form.is_valid():
             #get data entered by user
@@ -22,38 +24,65 @@ def login(request):
             password = form.cleaned_data['password']
             user_type = form.cleaned_data['user_type']
 
-            #sql query
+            #sql query to verify records
             try:
                 user = User.objects.get(user_name = name, email = email, user_type = user_type)
             except:
                 return redirect('login')
-            
-            return redirect('rides')
+
+            #check password
+            if password != user.password:
+               return redirect('login')
+
+            request.session['id'] = user.id;
+            return redirect('ride-create')
     #GET METHOD
     else:
         form = Login()
 
     return render(request, 'auth/login.html', {'form':form})
-        
+
+def logout(request):
+    del request.session['id']
+    return redirect('login')
+
 def register(request):
     if request.method == 'POST':
         form = Register(request.POST)
-        if form.is_valid():        
+        if form.is_valid():
+            
             #get data entered by user
             name = form.cleaned_data['user_name']
             email = form.cleaned_data['email']
             password = form.cleaned_data['password']
+            re_password = form.cleaned_data['re_passwd']
             user_type = form.cleaned_data['user_type']
 
+            if password != re_password:
+                redirect('register')
 
             #sql query
-            user = User(user_name = name, email = email, password = password, user_type = user_type)
-            user.save()
+            try:
+                user = User(user_name = name, email = email, password = password, user_type = user_type)
+                user.save()
+            except:
+                redirect('register')
             
             return redirect('login')
     else:
         form = Register()
-    return render(request, 'auth/reg.html', {'form':form})    
+
+    name = "None"
+    user_row = request.session.get('id', "None")
+    if user_row != "None":
+        user = User.objects.get(id = request.session['id'])
+        name = user.user_name
+        
+    context = {
+        'form' : form,
+        'name' : name,
+    }
+    return render(request, 'auth/reg.html', context)    
 
 
 class RideListView(ListView):
@@ -70,7 +99,6 @@ class RideCreateView(CreateView):
     model = Ride
     #context_object_name = 'new_ride'
     fields = ['arrival', 'destination', 'num_passengers', 'vehicle', 'shareable']
-    
     def form_valid(self, form):
         form.instance.owner = self.request.user
         return super().form_valid(form)
@@ -102,13 +130,18 @@ def open_rides(request):
 def request_ride(request):
     if request.method == 'POST':
         form = RequestRideForm(request.POST)
+        
     else:
         #thirty_mins = timezone.now() + datetime.timedelta(minutes=30)
         #form = RequestRideForm(initial={'arrival': thirty_mins})
         form = RequestRideForm()
 
+
+    
+    user = User.objects.get(id = request.session['id'])
     context = {
-        'form': form,
+        "form": form,
+        "name": "1234",
     }
 
     return render(request,'ride/request_ride.html', context)
