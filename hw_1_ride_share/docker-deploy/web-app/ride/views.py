5712073c804523af351777277f5227af700b5ac1
Gaunish <anigarg1999@gmail.com>
2022-01-29 09:00:16 -0500

    merge

--- hw_1_ride_share/docker-deploy/web-app/ride/views.py
+++ hw_1_ride_share/docker-deploy/web-app/ride/views.py
@@ -235,10 +235,10 @@ def open_rides(request):
 
 
 def request_ride(request):
-<<<<<<< HEAD
 #<<<<<<< HEAD
-=======
->>>>>>> b23f1160da5254b1dcd709ec3b7025f2469abe88
+#<<<<<<< HEAD
+#=======
+#>>>>>>> b23f1160da5254b1dcd709ec3b7025f2469abe88
     #check if user is logged in
     user = login_required(request)
     if user == False:
@@ -255,13 +255,13 @@ def request_ride(request):
     if user_row != "None":
         user_r = User.objects.get(id = request.session['id'])
         user = user_r.user_name
-<<<<<<< HEAD
+#<<<<<<< HEAD
 #=======
     user = User.objects.get(id = request.session['id'])
 #>>>>>>> 6d860299601862bd2c9b0f3c71d3d00e56bd0d74
-=======
+#=======
     user = User.objects.get(id = request.session['id'])
->>>>>>> b23f1160da5254b1dcd709ec3b7025f2469abe88
+#>>>>>>> b23f1160da5254b1dcd709ec3b7025f2469abe88
 
     if request.method == 'POST':
         form = RequestRideForm(request.POST)
