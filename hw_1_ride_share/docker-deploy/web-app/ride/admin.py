3e689e542d215d6d4412e545e9bea63b2ee201e8
ogg4 <ogg4@duke.edu>
2022-01-31 14:26:10 -0500

    Joining Ride as a sharer creates Rider instance

--- hw_1_ride_share/docker-deploy/web-app/ride/admin.py
+++ hw_1_ride_share/docker-deploy/web-app/ride/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Vehicle, User, Ride
+from .models import Vehicle, User, Ride, Rider
 
 admin.site.register(User)
 admin.site.register(Vehicle)
 admin.site.register(Ride)
+admin.site.register(Rider)
